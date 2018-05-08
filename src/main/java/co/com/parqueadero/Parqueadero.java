dc07a6a59d75b5aa5907875a232f641d6c405fc7
yeison.gomez <yeison.gomez@YEISON-GOMEZ.ceiba.local>
2018-05-08 09:11:00 -0500

    se adiciona el jacocoTestReport

---
+++ src/main/java/co/com/parqueadero/Parqueadero.java
@@ -0,0 +1,25 @@
+package co.com.parqueadero;
+
+public class Parqueadero {
+	private String test;
+	
+	public Parqueadero() {
+		super();
+	}
+	
+	public static String testear() {
+		return "test ejecutado correctamente";
+	}
+
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+
+}
